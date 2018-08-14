8aba0e58b9f05a6124a53a24aa5a5b7fd13f85b3
Xavier Provençal <xavier.provencal@etsmtl.ca>
2018-08-14 17:12:00 -0400

    Deserialization of Activation functions

---
+++ src/ActivationFunctions.cpp
@@ -0,0 +1,26 @@
+//-------------------------------------------------------------------------
+// Simple back-propagation neural network example
+// 2018 - Xavier Provençal
+// MIT license: https://opensource.org/licenses/MIT
+//-------------------------------------------------------------------------
+
+
+#include "ActivationFunctions.h"
+
+namespace BPN {
+
+    ActivationFunction* ActivationFunction::deserialize(const std::string& s)
+      {
+        if ( s.find("Sigmoid(") != std::string::npos )
+          {
+            double lambda = atof( s.substr( 8, s.size() - 9 ).c_str() );
+            return new Sigmoid(lambda);
+          }
+        if ( s.find("ReLU") != std::string::npos )
+          {
+            return new ReLU();
+          }
+        throw std::runtime_error("Unknown activation function");
+      }
+
+}
