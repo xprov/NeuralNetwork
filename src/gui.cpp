98ad685d839ad19aa4ec17ed477bc026ab1fcccf
Xavier Provençal <xavier.provencal@univ-savoie.fr>
2018-08-15 11:57:46 -0400

    Before switching to GooCanvas

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,21 +1,105 @@
 #include <gtk/gtk.h>
 #include <gdk/gdkcairo.h>
+#include <cairo/cairo.h>
 #include <iostream>
+#include <vector>
 
-
-static void print_hello (GtkWidget *widget, gpointer   data)
+struct Color
 {
-  (void) widget;
-  (void) data;
-  g_print ("Hello World\n");
+  static GdkRGBA black;
+  static GdkRGBA white;
+  static GdkRGBA grey;
+  static GdkRGBA red;
+  static GdkRGBA green;
+  static GdkRGBA blue;
+};
+
+GdkRGBA Color::black  = {0.0, 0.0, 0.0, 1.0};
+GdkRGBA Color::white  = {1.0, 1.0, 1.0, 1.0};
+GdkRGBA Color::grey   = {0.5, 0.5, 0.5, 1.0};
+GdkRGBA Color::red    = {1.0, 0.0, 0.0, 1.0};
+GdkRGBA Color::green  = {0.0, 1.0, 0.0, 1.0};
+GdkRGBA Color::blue   = {0.0, 0.0, 1.0, 1.0};
+
+struct Point
+{
+  Point( double x, double y ) : x(x), y(y) {}
+  double x;
+  double y;
+};
+
+class Rectangle 
+{
+public:
+  Rectangle( const Point& A, const Point& B ) 
+    : drawColor( Color::red )
+    , fillColor( Color::white )
+    {
+      minX = std::min( A.x ,B.x );
+      maxX = std::max( A.x ,B.x );
+      minY = std::min( A.y ,B.y );
+      maxY = std::max( A.y ,B.y );
+    }
+
+  Rectangle( const Rectangle& other ) 
+    : minX( other.minX )
+      , maxX( other.maxX )
+      , minY( other.minY )
+      , maxY( other.maxY )
+      , drawColor( other.drawColor )
+      , fillColor( other.fillColor )
+    { }
+
+  void drawSelf( cairo_t* cr )
+    {
+
+      cairo_set_source_rgb( cr, fillColor.red, fillColor.green, fillColor.blue  );
+      cairo_set_line_width( cr, 1 );
+
+      cairo_rectangle( cr, minX, minY, maxX, maxY );
+      cairo_fill_preserve( cr );
+      cairo_set_source_rgb( cr, drawColor.red, drawColor.green, drawColor.blue  );
+      cairo_stroke( cr );
+
+    }
+
+  void inverseFillColor()
+    {
+      fillColor.red   = 1.0 - fillColor.red;
+      fillColor.green = 1.0 - fillColor.green;
+      fillColor.blue  = 1.0 - fillColor.blue;
+    }
+
+  bool is_inside( const Point& p )
+    {
+      return ( minX <= p.x ) && ( p.x <= maxX ) && ( minY <= p.y ) && ( p.y <= maxY );
+    }
+
+  friend std::ostream& operator<<( std::ostream& os, const Rectangle& r );
+
+protected:
+  int minX, maxX, minY, maxY;
+  GdkRGBA drawColor;
+  GdkRGBA fillColor;
+};
+
+std::ostream& operator<<( std::ostream& os, const Rectangle& r )
+{
+  os << "Rectangle( (" << r.minX << ", " << r.minY << "), (" << r.maxX << ", " << r.maxY << ") )";
+  return os;
 }
 
-double d = 2.0;
-double inc = -0.1;
+     
 
 gboolean
 draw_callback (GtkWidget *widget, cairo_t *cr, gpointer data)
 {
+  std::vector<Rectangle>* v = (std::vector<Rectangle>*) data;
+
+  static double d = 2.0;
+  static double inc = -0.1;
+
+
   guint width, height;
   GdkRGBA color;
   GtkStyleContext *context;
@@ -38,6 +122,8 @@ draw_callback (GtkWidget *widget, cairo_t *cr, gpointer data)
       d += 2*inc;
     }
 
+
+
   gtk_style_context_get_color (context,
                                gtk_style_context_get_state (context),
                                &color);
@@ -45,16 +131,36 @@ draw_callback (GtkWidget *widget, cairo_t *cr, gpointer data)
 
   cairo_fill (cr);
 
+  for ( Rectangle r : *v ) 
+    {
+      r.drawSelf( cr );
+    }
+
+
  return FALSE;
 }
 
 static gboolean 
 clicked(GtkWidget *widget, GdkEventButton *event, gpointer user_data)
 {
-  (void) user_data;
+  std::vector<Rectangle>* v = (std::vector<Rectangle>*) user_data;
+  Point p( (int)event->x, (int)event->y );
+  std::cout << "widget " << widget << std::endl;
   std::cout << "click ("  << event->x << ", " << event->y << ")"<< std::endl;
   if ( event->button == 1 )
     {
+      for ( Rectangle& r : *v )
+        {
+          if ( r.is_inside( p ) )
+            {
+              std::cout << "Inside " << r << std::endl;
+              r.inverseFillColor();
+            }
+          else
+            {
+              std::cout << "Outside " << r << std::endl;
+            }
+        }
       gtk_widget_queue_draw(widget);
     }
   //draw_callback( widget, (cairo_t*) widget, NULL );
@@ -63,7 +169,7 @@ clicked(GtkWidget *widget, GdkEventButton *event, gpointer user_data)
 
 static void activate (GtkApplication *app, gpointer user_data)
 {
-  (void) user_data;
+  std::vector<Rectangle>* v = (std::vector<Rectangle>*) user_data;
   GtkWidget *window;
   GtkWidget *drawing_area;
 
@@ -76,9 +182,8 @@ static void activate (GtkApplication *app, gpointer user_data)
   drawing_area = gtk_drawing_area_new();
   gtk_widget_set_size_request (drawing_area, 700, 500);
   g_signal_connect (G_OBJECT (drawing_area), "draw",
-                    G_CALLBACK (draw_callback), NULL);
-  g_signal_connect(G_OBJECT(window), "button-press-event", G_CALLBACK(clicked), NULL);
-  //grid = gtk_grid_new ();
+                    G_CALLBACK (draw_callback), (gpointer) v );
+  g_signal_connect(G_OBJECT(window), "button-press-event", G_CALLBACK(clicked), (gpointer) v);
 
   /* Pack the container in the window */
   gtk_container_add (GTK_CONTAINER (window), drawing_area);
@@ -93,7 +198,12 @@ int main (int argc, char **argv)
   int status;
 
   app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+
+  std::vector<Rectangle> v; 
+  v.push_back( Rectangle( Point(10, 10), Point(50, 50) ) );
+  v.push_back( Rectangle( Point(310, 310), Point(350, 350) ) );
+
+  g_signal_connect (app, "activate", G_CALLBACK (activate), (gpointer) &v );
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
 
